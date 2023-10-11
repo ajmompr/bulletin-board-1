1c1ca93e59a8f0705cb90a4e16cf554a7a617b17
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-10-11 14:25:53 +0000

    added form for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,12 +23,12 @@ class PostsController < ApplicationController
     the_post.body = params.fetch("query_body")
     the_post.expires_on = params.fetch("query_expires_on")
     the_post.board_id = params.fetch("query_board_id")
-
+    pp the_post.valid?
     if the_post.valid?
       the_post.save
-      redirect_to("/posts", { :notice => "Post created successfully." })
+      redirect_to("/boards/#{the_post.board_id}", { :notice => "Post created successfully." })
     else
-      redirect_to("/posts", { :alert => the_post.errors.full_messages.to_sentence })
+      redirect_to("/boards/#{the_post.board_id}", { :alert => the_post.errors.full_messages.to_sentence })
     end
   end
 
